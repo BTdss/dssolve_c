ffd77ff1e9a4211533ba8caf1945b3e169aa7542
felipemarkson <47871564+felipemarkson@users.noreply.github.com>
2022-05-13 14:23:14 -0300

    Adding Build process

---
+++ .github/workflows/build.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+import platform
+
+
+FOLDER = "dssolve_c"
+sys_name = platform.system()
+
+os.system(f"cmake -DCMAKE_BUILD_TYPE=Release -S . -B build")
+os.system("cmake --build build")
+
+shutil.make_archive(f"{FOLDER}_{sys_name.lower()}_x64", 'zip', FOLDER)
