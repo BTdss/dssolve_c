a6ad7746159c47dcb4181fe3f00081a9104b4239
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2022-05-16 20:20:52 -0300

    Change the build process

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -8,9 +8,6 @@ sys_name = platform.system()
 
 os.system(f"cmake -DCMAKE_BUILD_TYPE=Release -S . -B build")
 
-if sys_name == "windows":
-    os.system("cmake --build build --config Release")
-else:
-    os.system("cmake --build build")
+os.system("cmake --build build --config Release")
 
 shutil.make_archive(f"{FOLDER}_{sys_name.lower()}_x64", 'zip', FOLDER)
