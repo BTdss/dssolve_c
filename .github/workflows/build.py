44634a8144d4f0ed01d9b63e46ceb9b7bfd9d1f8
Felipe Monteiro <47871564+felipemarkson@users.noreply.github.com>
2022-05-20 21:53:33 -0300

    Change from zip to gztar

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -10,4 +10,4 @@ os.system(f"cmake -DCMAKE_BUILD_TYPE=Release -S . -B build")
 
 os.system("cmake --build build --config Release")
 
-shutil.make_archive(f"{FOLDER}_{sys_name.lower()}_x64", 'zip', FOLDER)
+shutil.make_archive(f"{FOLDER}_{sys_name.lower()}_x64", 'gztar', FOLDER)
